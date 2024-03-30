6af7632bc02a264bed8b0f77089e0681d90a0116
miri-j-kim <miri.kim@ucalgary.ca>
2024-03-30 15:46:41 -0600

    .

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import SignupForm from './SignupForm.js';
 import {useNavigate} from 'react-router-dom';
 import { useAuthContext } from '../App.js';
